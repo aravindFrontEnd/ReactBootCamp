451927e0fb510c4dcd5d021b2cc6dcb869a8e050
Epsilon-Aravind <91867114+Epsilon-Aravind@users.noreply.github.com>
2022-11-17 20:56:32 +0530

    initial  commit

---
+++ Application.js
@@ -0,0 +1,13 @@
+
+
+const heading = React.createElement('h1',{},'Hello!! React via CDN');
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+
+root.render(heading)
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+"# ReactBootCamp day 1" 
